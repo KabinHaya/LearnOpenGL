4db85796a2461058164c69adb3ed1f2a93f4d0ad
KabinHaya <113222402+VaseHaya@users.noreply.github.com>
2025-11-14 02:46:45 +0800

    11-14 阴影映射

---
+++ src/5_03_DepthMap/DepthMap.cpp
@@ -0,0 +1,405 @@
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+#include <geometry/BoxGeometry.h>
+#include <geometry/SphereGeometry.h>
+#include <geometry/PlaneGeometry.h>
+#include <tools/shader.h>
+#include <tools/stb_image.h>
+#include <tools/camera.h>
+
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <format>
+#include <unordered_set>
+
+static void processInput(GLFWwindow* window);
+static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+static void mouseCallback(GLFWwindow* window, double posX, double posY);
+
+static unsigned int loadTexture(std::string_view path);
+static void renderScene(const Shader& shader);
+static void renderQuad();
+
+int SCREEN_WIDTH = 1280;
+int SCREEN_HEIGHT = 720;
+
+const unsigned int SHADOW_WIDTH = 1024;
+const unsigned int SHADOW_HEIGHT = 1024;
+
+// 摄像机
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+float lastX = SCREEN_WIDTH / 2.0f;
+float lastY = SCREEN_HEIGHT / 2.0f;
+bool isFirstMouse = true;
+bool isMouseCaptured = true; // 初始为捕获状态（隐藏鼠标，控制视角）
+
+// 时机
+float deltaTime = 0.0f; // 当前帧与上一帧的时间差
+float prevFrameTime = 0.0f; // 上一针的时间
+
+// 几何形状
+std::unique_ptr<BoxGeometry> cubeGeometry;
+std::unique_ptr<PlaneGeometry> planeGeometry;
+
+unsigned int quadVAO = 0;
+unsigned int quadVBO;
+
+int main()
+{
+
+    const char* glslVersion = "#version 330";
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // 这是创建的窗口
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    /*
+        回调函数注册
+        1.注册窗口变化监听
+        2.注册鼠标事件
+    */
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height)
+        {
+            glViewport(0, 0, width, height);
+        });
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetCursorPosCallback(window, mouseCallback);
+    glfwSetScrollCallback(window, [](GLFWwindow* window, double offsetX, double offsetY)
+    {
+        camera.ProcessMouseScroll(static_cast<float>(offsetY));
+    });
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // ------------------------------------------------------------
+    // 创建 imgui 上下文
+    ImGui::CreateContext();
+
+    // 设置样式
+    ImGui::StyleColorsDark();
+    // 设置平台和渲染器
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glslVersion);
+    // ------------------------------------------------------------
+
+    // 设置视口
+    // 从左下到右上
+    // 这是渲染窗口
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_PROGRAM_POINT_SIZE);
+
+    ImVec4 bgColor = ImVec4(0.12f, 0.12f, 0.15f, 1.0f);
+
+    Shader simpleDepthShader(SHADER_DIR "/shadowMappingDepth.vert", SHADER_DIR "/shadowMappingDepth.frag");
+    Shader debugDepthQuad(SHADER_DIR "/debugQuad.vert", SHADER_DIR "/debugQuad.frag");    
+
+    cubeGeometry  = std::make_unique<BoxGeometry>(1.0f, 1.0f, 1.0f);
+    planeGeometry = std::make_unique<PlaneGeometry>(1.0f, 1.0f);
+
+    unsigned int woodMap = loadTexture(ASSETS_DIR "/texture/wood.png");
+
+    // 为渲染的深度贴图创建一个帧缓冲对象
+    unsigned int depthMapFBO;
+    glGenFramebuffers(1, &depthMapFBO);
+    
+    // 创建一个2D纹理，提供给帧缓冲的深度缓冲使用
+    unsigned int depthMap;
+    glGenTextures(1, &depthMap);
+    glBindTexture(GL_TEXTURE_2D, depthMap);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,
+        SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr); // 纹理格式指定为GL_DEPTH_COMPONENT
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    // 把生成的深度纹理作为帧缓冲的深度缓冲
+    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+    glDrawBuffer(GL_NONE);
+    glReadBuffer(GL_NONE);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    debugDepthQuad.use();
+    debugDepthQuad.setInt("depthMap", 0);
+
+    glm::vec3 lightPos(-2.0f, 4.0f, -1.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        float currentFrameTime = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrameTime - prevFrameTime;
+        prevFrameTime = currentFrameTime;
+
+        // 开始 ImGui 帧
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        
+        ImGui::Begin("ImGui");
+            ImGui::Text("ESC: Exit  L: Lock/Unlock Cursor");
+            ImGui::Text("WASD: Movement  Space: Up  LCtrl: Down");
+            ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::Text("FOV: %.1f", camera.Zoom);
+            ImGui::Text("x: %.1f, y: %.1f, z: %.1f", camera.Position.x, camera.Position.y, camera.Position.z);
+            ImGui::Text("Actual resolution");
+            ImGui::SliderInt("Width", &SCREEN_WIDTH, 800, 1920);
+            ImGui::SliderInt("Height", &SCREEN_HEIGHT, 600, 1080);
+        ImGui::End();
+
+        // ------------------------------------------------------------
+        // 渲染指令
+        glClearColor(bgColor.x, bgColor.y, bgColor.z, bgColor.w);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        float nearPlane = 1.0f;
+        float farPlane  = 7.5f;
+        glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, nearPlane, farPlane);
+        glm::mat4 lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 lightSpaceMatrix = lightProjection * lightView;
+        
+        simpleDepthShader.use();
+        simpleDepthShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+
+        glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+            glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+            glClear(GL_DEPTH_BUFFER_BIT);
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, woodMap);
+            renderScene(simpleDepthShader);
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        // 重置视口大小
+        glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        debugDepthQuad.use();
+        debugDepthQuad.setFloat("nearPlane", nearPlane);
+        debugDepthQuad.setFloat("farPlane", farPlane);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, depthMap);
+        renderQuad();
+
+        // ImGui 渲染
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // 资源释放
+    cubeGeometry->dispose();
+    planeGeometry->dispose();
+    glDeleteVertexArrays(1, &quadVAO);
+    glDeleteBuffers(1, &quadVBO);
+
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    std::unordered_set<Camera_Movement> operations;
+
+    // 问题在于斜着走会更快，不过这个暂时不用考虑
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        operations.insert(Camera_Movement::FORWARD);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        operations.insert(Camera_Movement::BACKWARD);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        operations.insert(Camera_Movement::LEFT);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        operations.insert(Camera_Movement::RIGHT);
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        operations.insert(Camera_Movement::UP);
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+        operations.insert(Camera_Movement::DOWN);
+
+    if (!operations.empty())
+        camera.ProcessKeyboard(operations, deltaTime);
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_L && action == GLFW_PRESS)
+    { // 仅在按下瞬间触发
+        isMouseCaptured = !isMouseCaptured;
+        if (isMouseCaptured)
+        {
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+            isFirstMouse = true;
+        }
+        else
+        {
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        }
+    }
+}
+
+void mouseCallback(GLFWwindow* window, double posXIn, double posYIn)
+{
+    if (!isMouseCaptured)
+        return; // 如果鼠标未被捕获（即已释放），不处理视角移动
+
+    float posX = static_cast<float>(posXIn);
+    float posY = static_cast<float>(posYIn);
+
+    if (isFirstMouse)
+    {
+        lastX = posX;
+        lastY = posY;
+        isFirstMouse = false;
+    }
+
+    float offsetX = posX - lastX;
+    float offsetY = lastY - posY;
+
+    lastX = posX;
+    lastY = posY;
+
+    camera.ProcessMouseMovement(offsetX, offsetY);
+}
+
+
+
+unsigned int loadTexture(std::string_view path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    // 图像y轴翻转
+    stbi_set_flip_vertically_on_load(true);
+    int width, height, nrComponents;
+    unsigned char* data = stbi_load(path.data(), &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        /*
+            jpg 和 png格式不一样
+            jpg只有3个通道
+            png有4个通道，第4个通道设置透明度
+        */
+        GLenum format = GL_RGB;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+    else
+    {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+    }
+    stbi_image_free(data);
+
+    return textureID;
+}
+
+void renderScene(const Shader& shader)
+{
+    std::vector<glm::vec3> cubePositions
+    {
+        glm::vec3( 0.0f,  1.5f,  0.0f),
+        glm::vec3( 2.0f,  0.0f,  1.0f),
+        glm::vec3(-1.0f,  0.0f,  2.0f)
+    };
+
+    // floor
+    glBindVertexArray(planeGeometry->VAO);
+
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(0.0f, -0.51f, 0.0f));
+    model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    model = glm::scale(model, glm::vec3(10.0f));
+    shader.setMat4("model", model);
+    glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(planeGeometry->indices.size()), GL_UNSIGNED_INT, 0);
+    // cubes
+    glBindVertexArray(cubeGeometry->VAO);
+
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, cubePositions[0]);
+    model = glm::scale(model, glm::vec3(0.5f));
+    shader.setMat4("model", model);
+    glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(cubeGeometry->indices.size()), GL_UNSIGNED_INT, 0);
+
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, cubePositions[1]);
+    model = glm::scale(model, glm::vec3(0.5f));
+    shader.setMat4("model", model);
+    glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(cubeGeometry->indices.size()), GL_UNSIGNED_INT, 0);
+
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, cubePositions[2]);
+    model = glm::rotate(model, glm::radians(60.0f), glm::normalize(glm::vec3(1.0f, 0.0f, 1.0f)));
+    model = glm::scale(model, glm::vec3(0.25f));
+    shader.setMat4("model", model);
+    glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(cubeGeometry->indices.size()), GL_UNSIGNED_INT, 0);
+}
+
+void renderQuad()
+{
+    if (quadVAO == 0)
+    {
+        float quadVertices[] = {
+            // positions        // texture Coords
+            -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+             1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+             1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        };
+        // setup plane VAO
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    }
+    glBindVertexArray(quadVAO);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    glBindVertexArray(0);
+}
---
+++ src/5_03_DepthMap/shader/debugQuad.frag
@@ -0,0 +1,21 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D depthMap;
+uniform float nearPlane;
+uniform float farPlane;
+
+float LinearizeDepth(float depth)
+{
+	float z = depth * 2.0f - 1.0f;
+	return (2.0f * nearPlane * farPlane) / (farPlane + nearPlane - z * (farPlane - nearPlane));
+}
+
+void main()
+{
+	float depthValue = texture(depthMap, TexCoords).r;
+	// FragColor = vec4(vec3(LinearizeDepth(depthValue) / far_plane), 1.0); // perspective
+    FragColor = vec4(vec3(depthValue), 1.0); // orthographic
+}
